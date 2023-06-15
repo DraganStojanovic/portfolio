b09f3a5e28e6e8db58651d23391b4679d6311865
DraganStojanovic <draganstojanovic3@gmail.com>
2023-06-15 23:04:58 +0200

    Minor test changes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,7 +8,7 @@
     <title>Contact Page</title>
 </head>
 <body>
-<h1>THIS IS CONTACT PAGE from controller!!!</h1>
+<h1>THIS IS CONTACT PAGE from controller make wish!!!</h1>
 <a href="{{ route('about.page') }}"> About </a>
 </body>
 </html>
